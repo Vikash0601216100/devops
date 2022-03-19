9a981aca0f1cb0c83a820f298133d7738ede9dda
Vikash0601216100 <95157112+Vikash0601216100@users.noreply.github.com>
2022-03-20 04:08:23 +0530

    Update App.java

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "github chnage on repo..new change1.. ");
+        System.out.println( "github chnage on repo..new change10.. ");
     }
 }
